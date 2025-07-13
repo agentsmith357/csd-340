39153aa897439e7b849f778cf29c9dfe10c14ee7
Jelani Jenkins <jelani.jenkins@charter.com>
2025-07-13 19:38:54 -0400

    completed module 10.3
    Signed-off-by: Jelani Jenkins <jelani.jenkins@charter.com>

---
+++ module-10/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+      $('#neo').click(function() {
+        $('#output').text("You mean I can dodge bullets?");
+      });
+
+      $('#smith').click(function() {
+        $('#output').text("Mr. Anderson.  We missed you!");
+      });
+    });
